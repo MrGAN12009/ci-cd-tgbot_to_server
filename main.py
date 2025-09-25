5162b73162a14af76a81d09c2bdced5aa6b992d0
MrGAN12009 <mrgan12009@gmail.com>
2025-09-25 12:42:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+
+
+TOKEN = "8056530991:AAEgU6VIE6_SDFftjUXey_80AmZj7ANYpyI"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(f"Ты написал: {message.text}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
